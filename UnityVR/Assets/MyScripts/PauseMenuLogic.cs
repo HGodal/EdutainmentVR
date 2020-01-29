b840d0d57d4c395752522a591f6086f6a08e0402
kjetilsvihus <kjetilsvihus@me.com>
2020-01-29 13:04:21 +0100

    teleport, fade out, baderom, pauseknapp
    1. laget teleport (med restrictions)
    2. laget at det blir fadet til svart dersom spiller går inn i et objekt, eller veggene.
    3. laget baderom
    4. laget en pauseknapp (ikke ferdig med pausemenyen.

---
+++ UnityVR/Assets/MyScripts/PauseMenuLogic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Locomotion.Teleporters;
+using Zinnia.Data.Type;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PauseMenuLogic : MonoBehaviour
+{
+    public TeleporterFacade teleporter; //this will actually do our teleportation
+    public Transform playArea; //This is the area the teleportet moves
+    public Transform headOrientation;
+    public Transform pauseLocation; // here will the player be teleporteted to
+    public Transform gameLocation; // Here will the player be teleported to when exit pausemenu
+
+    protected bool inPauseMenu = false;
+
+    public List<GameObject> pauseItems; 
+    public List<GameObject> gameItems;
+
+    public GameObject teleportationRelease;
+    public GameObject teleportationPress;
+
+    public void SwitchRooms() {
+        // makes if statement to see where the player is when pressing menu button
+        TransformData teleportDestination = new TransformData(gameLocation);
+        if (!inPauseMenu) {
+            gameLocation.position = new Vector3(headOrientation.position.x, playArea.position.y, headOrientation.position.z);
+
+            Vector3 right = Vector3.Cross(playArea.up, headOrientation.forward);
+            Vector3 forward = Vector3.Cross(right, playArea.up);
+
+            gameLocation.rotation = Quaternion.LookRotation(forward, playArea.up);
+
+            teleportDestination = new TransformData(pauseLocation);
+        }
+
+        teleporter.Teleport(teleportDestination);
+        inPauseMenu = !inPauseMenu;
+
+        foreach (GameObject item in pauseItems) {
+            item.SetActive(inPauseMenu);
+        }
+
+        foreach (GameObject item in gameItems) {
+            item.SetActive(!inPauseMenu);
+        }
+    }
+    public void ResetGame() {
+        SceneManager.LoadScene("Final", LoadSceneMode.Single);
+    }
+
+    public void SwitchTeleportationToPress(bool value) {
+        teleportationRelease.SetActive(!value);
+        teleportationPress.SetActive(value);
+    }
+    public void SwitchTeleportationToRelease(bool value) {
+        SwitchTeleportationToPress(!value);
+    }
+}
