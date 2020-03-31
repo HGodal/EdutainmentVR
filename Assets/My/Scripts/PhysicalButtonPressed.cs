780869d1ed254479ea132a9594a81bb71215b264
Håvard <8015540+HGodal@users.noreply.github.com>
2020-03-31 20:58:47 +0200

    Poke Functionality

---
+++ Assets/My/Scripts/PhysicalButtonPressed.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicalButtonPressed : MonoBehaviour
+{
+    public UnityEvent onPressed;
+    protected bool lastState;
+
+    AudioSource test;
+
+    void Start()
+    {
+        lastState = false;
+        test = GetComponent<AudioSource>();
+    }
+
+    public void SetState(bool state)
+    {
+        if (state && !lastState && onPressed != null)
+        {
+            onPressed.Invoke();
+        }
+        lastState = state;
+    }
+
+    public void TestButton(){
+        test.Play();
+    }
+}
---
+++ Assets/My/Scripts/PokingGestureAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Zinnia.Action;
+
+public class PokingGestureAction : BooleanAction
+{
+    public Boolean gripTrigger { get; set; }
+    public Boolean indexTrigger { get; set; }
+    public Boolean isGrabbing { get; set; }
+
+    void Update()
+    {
+        Receive(gripTrigger && !indexTrigger && !isGrabbing);
+    }
+}
