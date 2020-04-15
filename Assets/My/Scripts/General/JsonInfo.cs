0bc3f9b8313c5512cc222f68c41620d95758a89f
Håvard <8015540+HGodal@users.noreply.github.com>
2020-04-15 14:46:54 +0200

    Update JsonInfo.cs

--- Assets/My/Scripts/General/JsonInfo.cs
+++ Assets/My/Scripts/General/JsonInfo.cs
@@ -3,8 +3,13 @@
 [System.Serializable]
 public class JsonInfo
 {
-    public string toolSorter;
-    public string pipeBuilder;
-    public string pipeFixer;
+    public string toolSorter1;
+    public string toolSorter2;
+    public string toolSorter3;
+    public string pipeBuilder1;
+    public string pipeBuilder2;
+    public string pipeFixer1;
+    public string pipeFixer2;
+
     public List<string> quizList;
 }
