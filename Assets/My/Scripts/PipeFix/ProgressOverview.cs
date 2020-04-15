4c046bc423d6c55eda675784a11ed35d76ae9c3b
Håvard <8015540+HGodal@users.noreply.github.com>
2020-04-15 14:46:49 +0200

    Moved Text to Json

--- Assets/My/Scripts/PipeFix/ProgressOverview.cs
+++ Assets/My/Scripts/PipeFix/ProgressOverview.cs
@@ -9,6 +9,7 @@ public class ProgressOverview : MonoBehaviour
     AudioSource victory;
     TextMeshProUGUI scoreText;
     CommonLogic commonLogic;
+    public GenerateJsonInfo allInfo;
 
     private void Start()
     {
@@ -23,22 +24,14 @@ public class ProgressOverview : MonoBehaviour
     {
         if (brokenPipes != 0)
         {
-            GetComponent<TextMeshProUGUI>().text = "Bak veggen på venstre side er det noen rør som lekker, \n" +
-                "       og du må fikse dem! \n\n" +
-                brokenPipes + " rør gjenstår! \n\n" +
-                "Bruk hammeren til å slå ned segmenter av veggen, og rørtangen \n" +
-                "        for å fikse de røde, ødelagte rørene. \n\n" +
-                "+10 poeng for å fikse et rør. \n" +
-                "-1 poeng for å ødelegge en bit av veggen.";
+            GetComponent<TextMeshProUGUI>().text = string.Format(allInfo.GetSceneInfo("pipeFixer1"), brokenPipes);
         }
         else
         {
             victory.Play();
             finished = true;
             StaticData.levelScores[3] = score;
-            GetComponent<TextMeshProUGUI>().text = "Gratulerer! \n\n" +
-                "Alle de ødelagte rørene er fikset, og vannet renner som det skal. \n\n" +
-                "Du vil bli teleportert tilbake til menyen om 5 sekund.";
+            GetComponent<TextMeshProUGUI>().text = allInfo.GetSceneInfo("pipeFixer2");
             commonLogic.WaitChangeScene(5.0f, "TheHub");
         }
         brokenPipes--;
