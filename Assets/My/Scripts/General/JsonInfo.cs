b3e45ff8c9d64a13c4b1ee6d3bb44ec96a67c9e4
Håvard <8015540+HGodal@users.noreply.github.com>
2020-04-15 10:46:39 +0200

    Created a way to read text from a JSON file

---
+++ Assets/My/Scripts/General/JsonInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class JsonInfo
+{
+    public string toolSorter;
+    public string pipeBuilder;
+    public string pipeFixer;
+    public List<string> quizList;
+}
